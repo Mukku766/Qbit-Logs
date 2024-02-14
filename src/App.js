bf66033642d4a0d2814b1ccf0899107bee885973
Mukku766 <jagdeeprrajput@gmail.com>
2024-02-14 17:17:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import LoginComponent from './Component/LoginComponent'; // Importing the LoginComponent
+import SignupComponent from './Component/SignupComponent'; // importing Signupcomponent
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ForgotComponent from './Component/ForgotComponent';
+import MenuComponent from './Component/MenuComponent';
+import ChangePasswordComponent from './Component/ChangePasswordComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <BrowserRouter>
+    <div className='flex flex-col h-full'>
+      <Routes>
+      <Route path='/' exact element={<LoginComponent/>}/>
+        <Route path='/register' exact element={<SignupComponent/>}/>
+        <Route path='/forgot' exact element={<ForgotComponent/>}/>
+        <Route path='/changepassword' exact element={<ChangePasswordComponent/>}/>
+        <Route path='/Home' exact element={<MenuComponent/>}/>
+
+      </Routes>
     </div>
+    
+      </BrowserRouter>
   );
 }
 
